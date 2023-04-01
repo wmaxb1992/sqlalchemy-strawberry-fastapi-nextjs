ebcc07147bf1d40f2b0e96ffd5d2a0fd7b72edcf
wmaxb1992 <66269844+wmaxb1992@users.noreply.github.com>
2023-04-01 02:39:52 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to fuck u<a href="https://nextjs.org">Next.js!</a>
+          Welcome to fuck u<a href="https://nextjs.org">Next.js! fucckkk</a>
         </h1>
 
         <p className={styles.description}>
